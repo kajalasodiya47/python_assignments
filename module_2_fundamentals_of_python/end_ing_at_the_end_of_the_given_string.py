18d3b3df224d7c2452184c6b3b12f2fc7b87a633
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-11 16:30:12 +0530

    Update end_ing_at_the_end_of_the_given_string.py

--- module_2_fundamentals_of_python/end_ing_at_the_end_of_the_given_string.py
+++ module_2_fundamentals_of_python/end_ing_at_the_end_of_the_given_string.py
@@ -1,8 +1,17 @@
+# enter string
 user_str = input("Enter a string:")
+'''
+  use ladder else if conditional statement 
+  
+''' 
+# if the string length of the given string is less than 3, leave it unchanged
 if len(user_str) < 3:
     result = user_str
+#  string already ends with 'ing' then add 'ly'    
 elif user_str.endswith('ing'):
     result = user_str + 'ly'
+# add 'ing' at the end of a given string    
 else:
     result = user_str + 'ing'
+# print the result    
 print("Result : ", result)    
