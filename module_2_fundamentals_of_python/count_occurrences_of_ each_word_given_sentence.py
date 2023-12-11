d558381a0e468b5b9b027d11984317645337539e
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-11 16:21:21 +0530

    Update count_occurrences_of_ each_word_given_sentence.py

--- module_2_fundamentals_of_python/count_occurrences_of_ each_word_given_sentence.py
+++ module_2_fundamentals_of_python/count_occurrences_of_ each_word_given_sentence.py
@@ -4,7 +4,10 @@ str1 = "To change the overall look your document. To change the look available i
 result = dict()
 # split() method splits string into a list
 word = str1.split(" ")
-
+'''
+      use get(key,value) dictionary method to return the value with +1
+      
+''' 
 for key in word:
     result[key] = result.get(key,0) + 1
 print(result) 
