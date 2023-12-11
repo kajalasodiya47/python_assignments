ca9062c2c2c94fa485b2b1b54e78626e06c3aca9
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-11 16:14:26 +0530

    Update count_no_of_chars_in_string.py

--- module_2_fundamentals_of_python/count_no_of_chars_in_string.py
+++ module_2_fundamentals_of_python/count_no_of_chars_in_string.py
@@ -1,5 +1,11 @@
+# enter string
 user_str = input("Enter a string : ")
+# empty dictionary
 res = {}
+# for loop
 for key in user_str:
+    '''
+      use get(key,value) dictionary method to return the value with +1
+    ''' 
     res[key] = res.get(key,0) + 1
 print(res)    
