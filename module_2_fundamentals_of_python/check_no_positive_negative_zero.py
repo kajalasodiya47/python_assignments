7f43c7da9ac7674bd91fa0206fd42704ab4980c8
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-11 16:09:19 +0530

    Update check_no_positive_negative_zero.py

--- module_2_fundamentals_of_python/check_no_positive_negative_zero.py
+++ module_2_fundamentals_of_python/check_no_positive_negative_zero.py
@@ -1,4 +1,8 @@
+# enter integer number
 num = int(input("Enter number:"))
+'''
+put the ladder else-if conditional statement to check number postive, negative or zero
+'''
 if num>0:
     print("Number is positive")
 elif num<0:
