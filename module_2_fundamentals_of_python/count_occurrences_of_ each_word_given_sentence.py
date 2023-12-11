e6336ea2fb86f2d30cb8d6838440947178769280
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-11 16:20:32 +0530

    Update count_occurrences_of_ each_word_given_sentence.py

--- module_2_fundamentals_of_python/count_occurrences_of_ each_word_given_sentence.py
+++ module_2_fundamentals_of_python/count_occurrences_of_ each_word_given_sentence.py
@@ -1,6 +1,10 @@
+# string
 str1 = "To change the overall look your document. To change the look available in the gallery"
+# empty dict() constructor
 result = dict()
+# split() method splits string into a list
 word = str1.split(" ")
+
 for key in word:
     result[key] = result.get(key,0) + 1
 print(result) 
