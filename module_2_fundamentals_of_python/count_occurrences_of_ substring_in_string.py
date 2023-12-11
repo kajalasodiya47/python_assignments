04271f060cf7464119561fd8186ff861004c984a
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-11 16:26:24 +0530

    Update count_occurrences_of_ substring_in_string.py

--- module_2_fundamentals_of_python/count_occurrences_of_ substring_in_string.py
+++ module_2_fundamentals_of_python/count_occurrences_of_ substring_in_string.py
@@ -1,3 +1,6 @@
+# string
 str = "Hello world! Hello everyone!"
+# string count() method returns the occurrences of the substring "Hello"
 substr = str.count("Hello")
+# print the result
 print(substr)
