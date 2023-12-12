e0ff039857bee47c266de08888d717bf9fac84d7
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 15:35:38 +0530

    Update vowel_or_not.py

--- module_2_fundamentals_of_python/vowel_or_not.py
+++ module_2_fundamentals_of_python/vowel_or_not.py
@@ -1,5 +1,10 @@
+# list letters
 letters = ['a','e','i','o','u','A','E','I','O','U']
+# input the alphabate
 alphabate = str(input("Enter an alphabate:"))
+'''
+  if entered alphabate in list letters, print vowel otherwise, print not vowel
+'''  
 if alphabate in letters:
     print("Vowel")
 else:
