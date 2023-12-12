78728f889e0cdc5817f18b46a47d880e15fd1e34
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 15:29:50 +0530

    Update swap_numbers.py

--- module_2_fundamentals_of_python/swap_numbers.py
+++ module_2_fundamentals_of_python/swap_numbers.py
@@ -1,17 +1,27 @@
+# swap two numbers with temp variable
+
 print("swap two number with temp variable:\n")
 a = 20
 b = 10
 print("Before swap values are:","a=",a,"b=",b)
-temp = a
-a = b
-b = temp
+temp = a  # value of a variable assigned to temp variable
+a = b     # and value of b variable assigned to a variable
+b = temp  # and then after value of temp variable assigned to b variable
+
+# after swapping the values are:
 print("After swap values are:","a=",a,"b=",b)
+
+
+
+# swap two numbers without temp variable
 
 print("\nswap two number without temp variable:\n")
 a = 20
 b = 10
 print("Before swap values are:","a=",a,"b=",b)
-a = a + b
-b = a - b
-a = a - b
+a = a + b  # addition of a and b variable, and the addition assigned to a variable
+b = a - b  # and subtraction of a and b variable, and the subtraction assigned to b variable
+a = a - b  # and then after subtraction of a and b variable, and the subtraction assigned to a variable
+
+# after swapping the values are:
 print("After swap values are:","a=",a,"b=",b)
