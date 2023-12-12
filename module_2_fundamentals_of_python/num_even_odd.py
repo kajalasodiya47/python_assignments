952091ec8261d02ff7aa07c9f8ba10849e614a91
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 15:41:30 +0530

    Update num_even_odd.py

--- module_2_fundamentals_of_python/num_even_odd.py
+++ module_2_fundamentals_of_python/num_even_odd.py
@@ -1,7 +1,7 @@
 # input the number
 num = int(input("Enter the number:"))
 '''
-  if given number modulo by 2 is equal to 0 given number is even otherwise given number is odd
+  if given number modulo by 2 is equal to 0,then given number is even otherwise given number is odd
 '''  
 if(num % 2 == 0):
    print("Number is even")
