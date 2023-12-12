eef89d93b4c56f8286c5a5e9fb011f99df2f89ec
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 15:15:03 +0530

    Update sum_of_first_n_positive_integers.py

--- module_2_fundamentals_of_python/sum_of_first_n_positive_integers.py
+++ module_2_fundamentals_of_python/sum_of_first_n_positive_integers.py
@@ -1,5 +1,9 @@
 sum = 0
+# input the number
 num = int(input("Enter number : "))
+'''
+   for loop starts from 1 to entered num + 1
+'''   
 for i in range(1,num+1):
-    sum = sum + i
-print("sum : ",sum)    
+    sum = sum + i   # when every iteration performs, sum = sum + i
+print("sum of first",num,"integers : ",sum)   # print the sum of first n positive integers  
