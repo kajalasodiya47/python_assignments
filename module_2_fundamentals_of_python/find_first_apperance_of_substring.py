17765a8ef9700677db0a8333c9bc511b8261df71
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 14:36:37 +0530

    Update find_first_apperance_of_substring.py

--- module_2_fundamentals_of_python/find_first_apperance_of_substring.py
+++ module_2_fundamentals_of_python/find_first_apperance_of_substring.py
@@ -1,8 +1,15 @@
+# string msg
 input_str = "The lyrics is not that poor!"
+'''
+  find() method find the first occurrence of not and poor substring
+'''  
 x = input_str.find('not')
 y = input_str.find('poor')
-if y > x:
-   input_str = input_str.replace(input_str[x:y+4],'good')
+'''
+   if poor substring first occurrence greater than not substring first occurrence replace the whole 'not'...'poor' substring with 'good otherwise returns the same as string
+'''   
+if y > x:  
+   input_str = input_str.replace(input_str[x:y+4],'good') # replace method replaces the whole 'not'...'poor' substring with 'good
    print(input_str)
 else:
    print(input_str)
