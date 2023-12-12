3feaa36406b02e68c7ec0e2fabde31c14d44e9e9
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 14:21:56 +0530

    Update find_factorial_number.py

--- module_2_fundamentals_of_python/find_factorial_number.py
+++ module_2_fundamentals_of_python/find_factorial_number.py
@@ -1,5 +1,9 @@
 fact=1
+# input the number to find factorial
 num=int(input("Enter the number to find factorial:"))
+'''
+  for loop starts from 1 to (entered the num + 1)
+'''  
 for i in range(1,num+1):
-    fact = fact * i
-print(fact)    
+    fact = fact * i   # in every iteration performs fact value multiply by i
+print(fact)    # print factorial of given number  
