715ba02d7fd5bdb7145b07d192f66f72718a8eed
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 15:19:54 +0530

    Update sum_of_three_integers.py

--- module_2_fundamentals_of_python/sum_of_three_integers.py
+++ module_2_fundamentals_of_python/sum_of_three_integers.py
@@ -1,6 +1,10 @@
+# input 1st, 2nd and 3rd number
 num1 = int(input("Enter 1st value : "))
 num2 = int(input("Enter 2nd value : "))
 num3 = int(input("Enter 3rd value : "))
+'''
+  if atleast 2 numbers are equal, sum will be zero otherwise performs the addition of given three numbers
+'''  
 if(num1 == num2 or num2 == num3 or num1 == num3):
     sum = 0
     print("sum : ",sum)
