2a751fb3d8f4a9576d43ea5678eeb32962f279ed
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 15:04:51 +0530

    Update get_string_made_of_first_and_last_two_chars.py

--- module_2_fundamentals_of_python/get_string_made_of_first_and_last_two_chars.py
+++ module_2_fundamentals_of_python/get_string_made_of_first_and_last_two_chars.py
@@ -1,7 +1,11 @@
+# input the string
 str = input("Enter a string : ")
+'''
+  if string length is less than 2 returns the empty string and if string length is greater than 2 returns the first 2 and last 2 chars from a given string using slicing string
+'''  
 if len(str) < 2:
     result = ""
 else:
     result = str[:2] + str[-2:]
-    
+# print the result    
 print("Result:",result)    
