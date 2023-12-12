197e19747fb5e4af7a594eb681302f8fea068d49
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 15:33:09 +0530

    Update two_int_values_check_equal_or_sum_diff_5.py

--- module_2_fundamentals_of_python/two_int_values_check_equal_or_sum_diff_5.py
+++ module_2_fundamentals_of_python/two_int_values_check_equal_or_sum_diff_5.py
@@ -1,5 +1,10 @@
+# input 1st number
 num1 = int(input("Enter 1st value : "))
+# input 2nd number
 num2 = int(input("Enter 2nd value : "))
+'''
+  if num1 and num2 are eqaul and addition of num1 and num2 are 5 and subtraction of num1 and num2 are 5 returns true otherwise returns false
+'''
 if(num1 == num2) or (num1 + num2 == 5) or (num1 - num2 == 5):
     print("true")
 else:
