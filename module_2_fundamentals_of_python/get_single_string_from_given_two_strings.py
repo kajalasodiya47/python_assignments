734961929f03e3689ef86f36666ca90d22f88e97
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-12-12 14:47:25 +0530

    Update get_single_string_from_given_two_strings.py

--- module_2_fundamentals_of_python/get_single_string_from_given_two_strings.py
+++ module_2_fundamentals_of_python/get_single_string_from_given_two_strings.py
@@ -1,6 +1,6 @@
 str1 = "kajal"
 str2 = "asodiya"
-x = str2[:1]+str1[1:]
-y = str1[:1]+str2[1:]
-str3 = x +" "+y
+x = str2[:1]+str1[1:]  #get the first character of str2 and get all the characters of str1 except first charcater 
+y = str1[:1]+str2[1:]  #get the first character of str1 and get all the characters of str2 except first charcater
+str3 = x +" "+y    # concates the x and y with space
 print(str3)
