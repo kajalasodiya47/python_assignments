52d724065a67cd3f55e7933de8343d0bc2a98162
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2024-01-10 13:46:34 +0530

    Update create_dictionary_using_tuples_in_python.py

--- Module_3_Collections_functions_and_Modules/create_dictionary_using_tuples_in_python.py
+++ Module_3_Collections_functions_and_Modules/create_dictionary_using_tuples_in_python.py
@@ -1,12 +1,5 @@
-# define function convert_dict with argument
-def convert_dict(tup, dic):  
-    #dict()  
-    dic = dict(tup)  
-    return dic
-
-# tuple    
-tuple_values = [("a", 1), ("b", 2), ("c", 3),("d", 4), ("e", 5), ("f", 6)]
-# empty dictionary
-res_dictionary = {}
-# call the function convert_dict  
-print ("The converted dictionary is: ", convert_dict(tuple_values,res_dictionary))  
+# list of tuples
+my_list = [(1,2),('a','b'),(4,'y')]
+# dict() constructor to convert list of tuples into dictionary
+my_dict = dict(my_list)
+print(my_dict)
