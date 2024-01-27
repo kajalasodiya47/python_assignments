9e2b19a1b836523eb5f695b4785bbfacde66b3fa
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2024-01-27 14:30:21 +0530

    Add files via upload

---
+++ Module_3_Collections_functions_and_Modules/traverse_through_dict_object.py
@@ -0,0 +1,9 @@
+# dictionary
+fruits = {
+    'name' : 'apple',
+    'colour' : 'red'
+    }
+# use for loop
+# use items() method to get key and value
+for key,value in fruits.items():
+    print(f'{key}={value}')
